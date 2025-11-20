84bf9543fc9a7c415cf557ad5581be5ec53fefd5
Laura Leciñena <lecinenaandres25@iespabloserrano.com>
2025-11-20 10:06:12 +0100

    Primera versión del programa

---
+++ src/proyecto_prueba/Primer_programa.java
@@ -0,0 +1,23 @@
+package proyecto_prueba;
+
+public class Primer_programa {
+
+	public static void main(String[] args) {
+		
+		int variable = 5;
+		variable ++;
+		variable = variable + 10;
+		
+		System.out.println( "Variable vale: " + variable );
+		
+		int contador = 0;
+		while ( contador < 10 ) {
+
+		System.out.println( "El número es: " + contador );
+		contador++;
+
+		}
+
+	}
+
+}
